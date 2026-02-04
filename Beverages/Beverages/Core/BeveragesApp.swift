149e6e5e52f8012dd5c6aa14fbe7d0c13f8ddd48
nidhupatel12-ops <nidhu.patel12@gmail.com>
2026-02-04 16:05:50 +0530

    Update BeveragesApp.swift

--- Beverages/Beverages/Core/BeveragesApp.swift
+++ Beverages/Beverages/Core/BeveragesApp.swift
@@ -1,16 +1,18 @@
-
 import SwiftUI
 import SwiftData
 
 @main
 struct BeveragesApp: App {
-    
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             BeverageEntity.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
+        if let url = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first {
+            if !FileManager.default.fileExists(atPath: url.path) {
+                try? FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
+            }
+        }
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
